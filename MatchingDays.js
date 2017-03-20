92d4524437b48c481ddf69ab7665dde036320848
ShanaSkydancer <shana.proverbs31@gmail.com>
2017-03-20 11:21:35 +0200

    Finished Matching Days exercise.

--- MatchingDays.js
+++ MatchingDays.js
@@ -1,7 +1,6 @@
 const form = document.getElementById('fillForm');
-var ul = document.getElementsByTagName('ul');
-var li = document.querySelectorAll('li');
-// var liList = document.getElementsByTagName('li');
+var ul = document.getElementById('weekdays');
+var day = document.querySelectorAll('day');
 var inputOne = document.getElementById('dateOne');
 var inputTwo = document.getElementById('dateTwo');
 var sumbitButt = document.getElementsByClassName('button');
@@ -11,35 +10,42 @@ var inputDateOne = new Date(inputDateOneString);
 var inputDateTwo = new Date(inputDateTwoString);
 var inputDayOne = inputDateOne.getDay();
 var inputDayTwo = inputDateOne.getDay();
-// var weekdays = ["Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-form.addEventListener("submit", function() {
+form.addEventListener('submit', function() {
   event.preventDefault();
   checkDate();
-});
+  });
 
+  function checkDate() {
+    var inputDateOneString = inputOne.value;
+    var inputDateTwoString = inputTwo.value;
+    var inputDateOne = new Date(inputDateOneString);
+    var inputDateTwo = new Date(inputDateTwoString);
+    var inputDayOne = inputDateOne.getDay();
+    var inputDayTwo = inputDateTwo.getDay();
+    var ul = document.getElementById('weekdays');
+    var li = ul.getElementsByTagName('li');
+      for (var i=0; i < li.length; i++){
+        var currentDay = i;
+        var currentDayColor = li[currentDay].getAttribute('class');
+        if (inputDayOne === currentDay){
+            // currentDayColor.className = "dateBlue";
+            li[currentDay].setAttribute('class', 'dateBlue');
+            // console.log(li.class);
+          }
+          if (inputDayTwo === currentDay){
+            // currentDayColor.className = "dateRed";
+            li[currentDay].setAttribute('class', 'dateRed');
 
-function checkDate() {
-  var inputDateOneString = inputOne.value;
-  var inputDateTwoString = inputTwo.value;
-  var inputDateOne = new Date(inputDateOneString);
-  var inputDateTwo = new Date(inputDateTwoString);
-  var inputDayOne = inputDateOne.getDay();
-  var inputDayTwo = inputDateOne.getDay();
-  // var li = document.querySelectorAll('li');
-  var liList = ul.getElementsByTagName('li');
-  for (var i=0; i < liList.length; i++){
-    if (inputDateOne == liList){
-      liList.dateBlue;
-    }
-    else if (inputDayTwo == liList){
-      liList.dateRed;
-    }
-    else if (inputDateOne == liList && inputDayTwo == liList){
-      liList.dateGreen;
-    }
-    else if (inputDateOne != liList || inputDayTwo != liList) {
-      liList.dateNormal;
-    }
+          }
+          if (inputDayOne === currentDay && inputDayTwo === currentDay){
+            // currentDayColor.className = "dateGreen";
+            li[currentDay].setAttribute('class', 'dateGreen');
+          }
+          else if (inputDayOne !== currentDay && inputDayTwo !== currentDay) {
+            // currentDayColor.className = "dateNormal";
+            li[currentDay].setAttribute('class', 'dateNormal');
+
+          }
+      }
   }
-}
