3431494062d9e536d46e41faa2cef0d9018f99e2
ShanaSkydancer <shana.proverbs31@gmail.com>
2017-05-29 11:14:20 +0200

    Clean code

--- checkDate.js
+++ checkDate.js
@@ -1,3 +1,4 @@
+//Checks two different dates to see if they fall under the same day
 function checkDate() {
   var inputDateOneString = inputOne.value;
   var inputDateTwoString = inputTwo.value;
@@ -7,27 +8,28 @@ function checkDate() {
   var inputDayTwo = inputDateTwo.getDay();
   var ul = document.getElementById('weekdays');
   var li = ul.getElementsByTagName('li');
-    for (var i=0; i < li.length; i++){
-      var currentDay = i;
-      var currentDayColor = li[currentDay].getAttribute('class');
-      if (inputDayOne === currentDay){
-          // currentDayColor.className = "dateBlue";
-          li[currentDay].setAttribute('class', 'dateBlue');
-          // console.log(li.class);
-        }
-        if (inputDayTwo === currentDay){
-          // currentDayColor.className = "dateRed";
-          li[currentDay].setAttribute('class', 'dateRed');
-
-        }
-        if (inputDayOne === currentDay && inputDayTwo === currentDay){
-          // currentDayColor.className = "dateGreen";
-          li[currentDay].setAttribute('class', 'dateGreen');
-        }
-        else if (inputDayOne !== currentDay && inputDayTwo !== currentDay) {
-          // currentDayColor.className = "dateNormal";
-          li[currentDay].setAttribute('class', 'dateNormal');
-
-        }
+  for (var i = 0; i < li.length; i++) {
+    var currentDay = i;
+    var currentDayColor = li[currentDay].getAttribute('class');
+    //Sets the first date background to Blue
+    if (inputDayOne === currentDay) {
+      // currentDayColor.className = "dateBlue";
+      li[currentDay].setAttribute('class', 'dateBlue');
     }
+    //Sets the second date background to Red
+    if (inputDayTwo === currentDay) {
+      // currentDayColor.className = "dateRed";
+      li[currentDay].setAttribute('class', 'dateRed');
+    }
+    //If both dates fall under the same day, it sets the date's background to Green
+    if (inputDayOne === currentDay && inputDayTwo === currentDay) {
+      // currentDayColor.className = "dateGreen";
+      li[currentDay].setAttribute('class', 'dateGreen');
+    }
+    //Resets the color of the background to 'Normal'
+    else if (inputDayOne !== currentDay && inputDayTwo !== currentDay) {
+      // currentDayColor.className = "dateNormal";
+      li[currentDay].setAttribute('class', 'dateNormal');
+    }
+  }
 }
--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var inputOne = document.getElementById('dateOne');
 var inputTwo = document.getElementById('dateTwo');
 var sumbitButt = document.getElementsByClassName('button');
 
+//Listens for when the form is submitted
 form.addEventListener('submit', function() {
   event.preventDefault();
   checkDate();
